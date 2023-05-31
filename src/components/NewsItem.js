3d512100d4327e536c98f4c0b4dbe7d6c98039a9
rawani123 <121213641+rawani123@users.noreply.github.com>
2023-05-31 19:49:01 +0530

    Third commit

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,11 +7,11 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageurl} className="card-img-top" alt="..." />
+          <img src={!imageurl?"https://techcrunch.com/wp-content/uploads/2021/12/android-spyware2.jpg?resize=1200,674":imageurl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
-          <a href={newsurl} target="_blank" className="btn btn-sm btn-primary">
+          <a href={newsurl} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-primary">
               Read More
             </a>
           </div>
